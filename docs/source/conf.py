fc37a655f913db359a9c69761406d12835cff7cb
florije <florije4ex@gmail.com>
2022-10-10 09:50:54 +0800

    modify sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,7 @@
 import os
 import sys
 import myst_parser
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../..'))
 
@@ -19,7 +20,12 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    "myst_parser",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -29,5 +35,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
