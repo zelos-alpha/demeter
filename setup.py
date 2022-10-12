77e15b43cd2efeecf104dbc3a3befc71b5bd8aab
32ethers <somereasons@outlook.com>
2022-10-12 15:03:51 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,16 @@ setup(
     ],
     name='demeter',
     version='0.1.0',
-    packages=find_packages(include=["demeter"], exclude=["tests", "samples"]),
-    # packages=["demeter"],
-    # package_dir={'demeter': './demeter'},
+    packages=find_packages(exclude=["tests", "tests.*", "samples", "samples.*"]),
+    # packages=["demeter", ],  # "broker" "core", "download", "indicator", "strategy", "utils"
+    # package_dir={'demeter': 'demeter'
+    #              # 'broker': './demeter/broker',
+    #              # 'core': './demeter/core',
+    #              # 'download': './demeter/download',
+    #              # 'indicator': './demeter/indicator',
+    #              # 'strategy': './demeter/strategy',
+    #              # 'utils': './demeter/utils',
+    #              },
     url='https://zelos-demeter.readthedocs.io',
     license='MIT',
     author='zelos team',
@@ -17,22 +24,7 @@ setup(
     description='better back testing tool for uniswap v3',
     python_requires='>=3.10',
     install_requires=["pandas>=1.4.4", "tqdm>=4.64.1"],
-    # data_files=[
-    #     ('', ['conf/*.conf']),
-    #     ('/usr/lib/systemd/system/', ['bin/*.service']),
-    # ],
 
-    # package_data={
-    #     '': ['*.txt'],
-    #     'bandwidth_reporter': ['*.txt']
-    # },
-    # exclude_package_data={
-    #     'data': ['*.*'],
-    #     'auth': ['*.json'],
-    #     'tests': ['*.*'],
-    #     'samples': ['*.*'],
-    #     'demeter.egg-info': ['*.*'],
-    # }
 )
 
-# python setup.py sdist upload -r private
+# rm -rf ./demeter.egg-info/ & python setup.py sdist upload -r private
