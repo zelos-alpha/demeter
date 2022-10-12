d801bad75a9d82fa26c4a9fb46e5868dd6ba2580
florije <florije4ex@gmail.com>
2022-10-12 20:57:25 +0800

    modify setup project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='demeter',
+    name='zelos-demeter',
     version='0.1.0',
     packages=find_packages(exclude=["tests", "tests.*", "samples", "samples.*"]),
     url='https://zelos-demeter.readthedocs.io',
