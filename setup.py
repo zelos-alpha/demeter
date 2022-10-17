a8cf8a906d78e80d3edf0399fabf19ba559545cd
florije <florije4ex@gmail.com>
2022-10-17 13:14:10 +0800

    add pypi readme content

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='zelos-demeter',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=["tests", "tests.*", "samples", "samples.*"]),
     url='https://zelos-demeter.readthedocs.io',
     license='MIT',
     author='zelos team',
     author_email='liang.hou@antalpha.com',
     description='better back testing tool for uniswap v3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires='>=3.10',
     install_requires=["pandas>=1.4.4",
                       "tqdm>=4.64.1",
