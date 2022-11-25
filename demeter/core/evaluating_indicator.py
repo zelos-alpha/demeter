bdf54d73182ea7ee8dd535c3ed3f635a5eb8d512
32ethers <somereasons@outlook.com>
2022-11-25 17:30:13 +0800

    optimize max drawdown

--- demeter/core/evaluating_indicator.py
+++ demeter/core/evaluating_indicator.py
@@ -26,7 +26,7 @@ class Evaluator(object):
         self._evaluating_indicator = EvaluatingIndicator(DECIMAL_ZERO, DECIMAL_ZERO, DECIMAL_ZERO)
         self._evaluating_indicator.annualized_returns = UnitDecimal(self.get_annualized_returns(), "")
         self._evaluating_indicator.benchmark_returns = UnitDecimal(self.get_benchmark_returns(), "")
-        self._evaluating_indicator.max_drawdown = UnitDecimal(Evaluator.get_max_drawdown(self.data.net_value), "")
+        self._evaluating_indicator.max_drawdown = UnitDecimal(Evaluator.get_max_drawdown_fast(self.data.net_value), "")
         return self._evaluating_indicator
 
     def get_annualized_returns(self):
@@ -45,6 +45,7 @@ class Evaluator(object):
 
     @staticmethod
     def get_max_drawdown(net_values: pd.Series):
+
         net_values.index = range(len(net_values.index))  # restruct index to access faster
         max_drawdown = 0
         for index, row in net_values.iteritems():
@@ -52,6 +53,36 @@ class Evaluator(object):
             if current_max > max_drawdown:
                 max_drawdown = current_max
         return max_drawdown
+
+    @staticmethod
+    def get_max_drawdown_fast(net_values: pd.Series):
+        max_value, idx_h, idx_l = Evaluator._withdraw_with_high_low(net_values.to_list())
+        return (net_values.iloc[idx_h] - net_values.iloc[idx_l]) / net_values.iloc[idx_h]
+
+    @staticmethod
+    def _withdraw_with_high_low(arr: list):
+        """
+        from : https://blog.csdn.net/Spade_/article/details/112341428
+        """
+
+        # 传入一个数组，返回最大回撤和对应的最高点索引、最低点索引
+        _dp = 0  # 使用 _dp 表示 i 点的最大回撤
+        i_high = 0  # 遍历时，0 ~ i - 1 中最高的点的索引，注意是索引
+
+        # 全局最大回撤和对应的最高点和最低点的索引，注意是索引
+        g_withdraw, g_high, g_low = float('-inf'), -1, -1
+
+        for i in range(1, len(arr)):
+            if arr[i_high] < arr[i - 1]:  # 若 0 ~ i - 1 中最高的点小于当前点
+                i_high = i - 1  # 0 ~ i - 1 中最高的点的索引
+
+            _dp = arr[i_high] - arr[i]  # _dp 表示 i 点的最大回撤
+            if _dp > g_withdraw:  # 找到新的最大回撤，更新三个值
+                g_withdraw = _dp
+                g_high = i_high
+                g_low = i
+
+        return g_withdraw, g_high, g_low
 
     def __get_benchmark_asset(self):
         base_amount = self.init_net_value / 2
--- tests/evaluating_indicator_test.py
+++ tests/evaluating_indicator_test.py
@@ -1,15 +1,31 @@
 import unittest
 import pandas as pd
-
+import time
 from demeter.core.evaluating_indicator import Evaluator
 
 
 class EvaluatorTest(unittest.TestCase):
     def test_max_withdraw(self):
-        DATA_SIZE = 500
-        index = pd.date_range('2022-10-8 8:0:0', periods=DATA_SIZE, freq='T')
+        DATA_SIZE = 100000
         data = range(DATA_SIZE, 0, -1)
-        data = pd.Series(data=data, index=index)
+        data = pd.Series(data=data)
+        t1 = time.time()
+        mw = Evaluator.get_max_drawdown_fast(data)
+
+        print(f"result: {mw}, time : {time.time() - t1}s")
+        self.assertEqual(mw, (data.iloc[0] - data.iloc[len(data.index) - 1]) / data.iloc[0])
+
+    def test_max_withdraw_fast(self):
+        DATA_SIZE = 100000
+        data = range(DATA_SIZE, 0, -1)
+        data = pd.Series(data=data)
+        t1 = time.time()
+        mw = Evaluator._withdraw_with_high_low(data.to_list())
+        r = (data.iloc[mw[1]] - data.iloc[mw[2]]) / data.iloc[mw[1]]
+        print(f"func out: {mw}, result: {r}, time : {time.time() - t1}s")
+        self.assertEqual(r, (data.iloc[0] - data.iloc[len(data.index) - 1]) / data.iloc[0])
+
+    def test_max_withdraw(self):
+        data = pd.Series(data=[3, 1, 8, 5, 6, 2, 9, 4, 5])
         mw = Evaluator.get_max_drawdown(data)
-        print(mw)
-        self.assertEqual(mw, 0.998)
+        self.assertEqual(mw, (8 - 2) / 8)
