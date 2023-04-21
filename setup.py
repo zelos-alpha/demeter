83f854dd00c0a23996a32578fc844accd350f2b4
florije <florije4ex@gmail.com>
2023-04-21 15:09:47 +0800

    release 0.2.1 version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='zelos-demeter',
-    version='0.1.10',
+    version='0.2.1',
     packages=find_packages(exclude=["tests", "tests.*", "samples", "samples.*"]),
     url='https://zelos-demeter.readthedocs.io',
     license='MIT',
