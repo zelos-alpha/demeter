5da004be8111ef6bbf4058e578c3c1aa2c6735a4
32ethers <somereasons@outlook.com>
2025-01-17 11:48:21 +0800

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'Demeter'
 copyright = '2024, zelos research'
 author = 'zelos research'
-release = '0.8.2'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
